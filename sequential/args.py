1b0432e94736053e0e9d7726ec7be029d41677d1
yeon0729 <yeon0729@sookmyung.ac.kr>
2023-05-06 07:56:45 +0000

    feat: load and encode data

--- sequential/args.py
+++ sequential/args.py
@@ -3,11 +3,16 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--seed", default=42, type=int, help="set seed number")
+    parser.add_argument("--seed", default=42, type=int, help="seed number")
     parser.add_argument("--device", default="cpu", type=str, help="cpu or gpu")
-    parser.add_argument("--path", default="/opt/ml/input/data/", type=str, help="data path to load")
+    parser.add_argument("--data_path", default="/opt/ml/input/data/", type=str, help="csvdata path")
+    parser.add_argument("--asset_path", default="asset/", type=str, help="asset data path")
+
+    parser.add_argument("--train_file", default="train_data.csv", type=str, help="train file name")
+    parser.add_argument("--test_file", default="test_data.csv", type=str, help="test file name")
 
     parser.add_argument("--epoch", default=10, type=int, help="epochs to run")
+    parser.add_argument("--batch_size", default=64, type=int, help="mini batch szie")
 
     args = parser.parse_args()
 
--- sequential/dataloader.py
+++ sequential/dataloader.py
@@ -6,25 +6,56 @@ import torch
 import pytorch_lightning as pl
 from sklearn.preprocessing import LabelEncoder
 
-class DKTDataModule(pl.LightningDataModule):
-    def __init__(self, batch_size):
-        super().__init__()
-        self.batch_size = batch_size
-        # define preprocess func as variable at here
 
-    # load or download dataset
-    def prepare_data(self):
+class DKTDataModule(pl.LightningDataModule):
+    def __init__(self, args):
+        super().__init__()
+        self.args = args
+        self.df = pd.DataFrame()
+    
+    # Fill feature engineering func if needed using self.df
+    def __feature_engineering(self):
         pass
 
-    # define dataset and preprocess on train/val or test case
+    # Encode and Save/Load data
+    def __preprocessing(self, is_train: bool = True):
+        cate_cols = ["assessmentItemID", "testId", "KnowledgeTag"]
+
+        if not os.path.exists(self.args.asset_path):
+            os.makedirs(self.args.asset_path)
+
+        for col in cate_cols:
+            le = LabelEncoder()
+
+            if is_train:
+                a = self.df[col].unique().tolist() + ["unknown"]
+                le.fit(a)  # encode str to int(0~N)
+                le_path = os.path.join(self.args.asset_path, col + "_classes.npy")
+                np.save(le_path, le.classes_)  # save encoded data
+            else:
+                le_path = os.path.join(self.args.asset_path, col + "_classes.npy")
+                le.classes_ = np.load(le_path)
+                self.df[col] = self.df[col].apply(lambda x: x if str(x) in le.classes_ else "unknown")
+
+            self.df[col] = self.df[col].astype(str)
+            test = le.transform(self.df[col])
+            self.df[col] = test
+
+    # load and feature_engineering dataset
+    def prepare_data(self):
+        train_file_path = os.path.join(self.args.data_path, self.args.train_file)
+        self.df = pd.read_csv(train_file_path)
+        self.__feature_engineering()  # fill this func if needed
+
+    # preprocess and set dataset on train/test case
     def setup(self, stage = None):
         if stage == "fit" or stage is None:
-            pass 
+            self.df = self.__preprocessing(is_train=True)
         if stage == "test" or stage is None:
-            pass
-    
-
-    # define dataloader of each case
+            self.df = self.__preprocessing(is_train=False)
+        
+        # self.args_nqueionts
+            
     def train_dataloader():
         # return DataLoder()
         pass
