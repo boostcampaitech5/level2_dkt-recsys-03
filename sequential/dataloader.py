00151e81de44d7e6845ae5be2674507c94f66c55
yeon0729 <yeon0729@sookmyung.ac.kr>
2023-05-07 07:26:16 +0000

    feat: add test_data in proprocess

--- sequential/dataloader.py
+++ sequential/dataloader.py
@@ -59,6 +59,7 @@ class DKTDataModule(pl.LightningDataModule):
         super().__init__()
         self.args = args
         self.df = pd.DataFrame()
+        self.test_df = pd.DataFrame()
 
         self.train_data = None
         self.valid_data = None
@@ -66,13 +67,19 @@ class DKTDataModule(pl.LightningDataModule):
 
         self.pin_memory = False
     
-    # Fill feature engineering func if needed using self.df
+    # Fill feature engineering func if needed using self.df, self.test_df
     def __feature_engineering(self):
         pass
 
     # Encode and Save/Load data
     def __preprocessing(self, is_train: bool = True):
         cate_cols = ["assessmentItemID", "testId", "KnowledgeTag"]
+        print(">>>>> Now is_train of preprocess is", is_train)
+
+        # convert time data to int timestamp
+        def convert_time(s: str):
+            timestamp = time.mktime(datetime.strptime(s, "%Y-%m-%d %H:%M:%S").timetuple())
+            return int(timestamp)
 
         if not os.path.exists(self.args.asset_path):
             os.makedirs(self.args.asset_path)
@@ -85,20 +92,24 @@ class DKTDataModule(pl.LightningDataModule):
                 le.fit(a)  # encode str to int(0~N)
                 le_path = os.path.join(self.args.asset_path, col + "_classes.npy")
                 np.save(le_path, le.classes_)  # save encoded data
+
+                self.df[col] = self.df[col].astype(str)
+                test = le.transform(self.df[col])
+                self.df[col] = test
             else:
                 le_path = os.path.join(self.args.asset_path, col + "_classes.npy")
                 le.classes_ = np.load(le_path)
-                self.df[col] = self.df[col].apply(lambda x: x if str(x) in le.classes_ else "unknown")
+                self.test_df[col] = self.test_df[col].apply(lambda x: x if str(x) in le.classes_ else "unknown")
 
-            self.df[col] = self.df[col].astype(str)
-            test = le.transform(self.df[col])
-            self.df[col] = test
+                self.test_df[col] = self.test_df[col].astype(str)
+                test = le.transform(self.test_df[col])
+                self.test_df[col] = test
 
-        def convert_time(s: str):
-            timestamp = time.mktime(datetime.strptime(s, "%Y-%m-%d %H:%M:%S").timetuple())
-            return int(timestamp)
+        if is_train:
+            self.df["Timestamp"] = self.df["Timestamp"].apply(convert_time)
+        else:
+            self.test_df["Timestamp"] = self.test_df["Timestamp"].apply(convert_time)
 
-        self.df["Timestamp"] = self.df["Timestamp"].apply(convert_time)
 
     # split train data to train & valid : this part will be excahnged
     def split_data(self, data: np.ndarray, ratio: float = 0.7, shuffle: bool = True, seed: int = 42):
@@ -115,47 +126,60 @@ class DKTDataModule(pl.LightningDataModule):
     # load and feature_engineering dataset
     def prepare_data(self):
         train_file_path = os.path.join(self.args.data_path, self.args.train_file)
+        test_file_path = os.path.join(self.args.data_path, self.args.test_file)
+
         self.df = pd.read_csv(train_file_path)
+        self.test_df = pd.read_csv(test_file_path)
         self.__feature_engineering()  # fill this func if needed
 
     # preprocess and set dataset on train/test case
     def setup(self, stage = None):
         if stage == "fit" or stage is None:
             self.__preprocessing()
-        # if stage == "test" or stage is None:
-        #     self.__preprocessing(is_train=False)
+        if stage == "test" or stage is None:
+            self.__preprocessing(is_train=False)
         
         self.args.n_questions = len(np.load(os.path.join(self.args.asset_path, "assessmentItemID_classes.npy")))
         self.args.n_tests = len(np.load(os.path.join(self.args.asset_path, "testId_classes.npy")))
         self.args.n_tags = len(np.load(os.path.join(self.args.asset_path, "KnowledgeTag_classes.npy")))
 
-        self.df = self.df.sort_values(by=["userID","Timestamp"], axis=0)
         columns = ["userID", "assessmentItemID", "testId", "answerCode", "KnowledgeTag"]
-        group = self.df[columns].groupby("userID").apply(
-            lambda r: (
-                r["testId"].values,
-                r["assessmentItemID"].values,
-                r["KnowledgeTag"].values,
-                r["answerCode"].values,
-            )
-        )
 
         if stage == "fit" or stage is None:
+            self.df = self.df.sort_values(by=["userID","Timestamp"], axis=0)
+            group = self.df[columns].groupby("userID").apply(
+                lambda r: (
+                    r["testId"].values,
+                    r["assessmentItemID"].values,
+                    r["KnowledgeTag"].values,
+                    r["answerCode"].values,
+                )   
+            )
             self.split_data(group.values)
-        # if stage == "test" or stage is None:
-        #     self.test_data = group.values
+
+        if stage == "test" or stage is None:
+            self.test_df = self.test_df.sort_values(by=["userID","Timestamp"], axis=0)
+            group = self.test_df[columns].groupby("userID").apply(
+                lambda r: (
+                    r["testId"].values,
+                    r["assessmentItemID"].values,
+                    r["KnowledgeTag"].values,
+                    r["answerCode"].values,
+                )   
+            )
+            self.test_data = group.values
 
     def train_dataloader(self):
         trainset = DKTDataset(self.train_data, self.args)
         print("trainset: ", len(trainset))
-        return DataLoader(trainset, batch_size=self.args.batch_size, shuffle=True, num_worker=self.args.num_workers, pin_memory=self.pin_memory)
+        return DataLoader(trainset, batch_size=self.args.batch_size, shuffle=True, num_workers=self.args.num_workers, pin_memory=self.pin_memory)
 
     def val_dataloader(self):
         valset = DKTDataset(self.valid_data, self.args)
         print("valset: ", len(valset))
-        return DataLoader(valset, batch_size=self.args.batch_size, shuffle=False, num_worker=self.args.num_workers, pin_memory=self.pin_memory)
+        return DataLoader(valset, batch_size=self.args.batch_size, shuffle=False, num_workers=self.args.num_workers, pin_memory=self.pin_memory)
 
-    # def test_dataloader(self):
-    #     testset = DKTDataset(self.test_data, self.args)
-    #     print("testset: ", len(testset))
-    #     return DataLoader(testset, batch_size=self.args.batch_size, shuffle=False, num_worker=self.args.num_workers, pin_memory=self.pin_memory)
+    def test_dataloader(self):
+        testset = DKTDataset(self.test_data, self.args)
+        print("testset: ", len(testset))
+        return DataLoader(testset, batch_size=self.args.batch_size, shuffle=False, num_workers=self.args.num_workers, pin_memory=self.pin_memory)
