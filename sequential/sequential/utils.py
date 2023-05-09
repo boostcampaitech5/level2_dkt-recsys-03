7ef8e1cd82d05d01e247432495b9b91abbb10a57
yeon0729 <yeon0729@sookmyung.ac.kr>
2023-05-09 13:36:00 +0000

    chore: move files directory

---
+++ sequential/sequential/utils.py
@@ -0,0 +1,30 @@
+import os
+import random
+import numpy as np
+import torch
+
+
+def set_seeds(seed: int = 42):
+    # set random seed
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+def get_logger():
+    import logging
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)  # set logging level
+
+    # set logging foramt
+    formatter = logging.formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    
+    # set print log
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    return logger
