cc5c4b9c883e0dabbeac95eeaa9822011bd7c176
juhyein <hyeinju304@gmail.com>
2023-05-13 07:44:11 +0000

    refactor: seperate trainer from main

--- graph/main.py
+++ graph/main.py
@@ -1,29 +1,10 @@
-import lightning as L
-import pandas as pd
-import os
-from model import LightGCNNet
-from datetime import datetime
-from dataloader import GraphDataModule
-
+from graph.trainer import Trainer
+from graph.dataloader import GraphDataModule
 
 def main():
-    datamodule = GraphDataModule()
-    model = LightGCNNet()
-    trainer = L.Trainer(max_epochs=10, log_every_n_steps=1)
-    trainer.fit(model,datamodule=datamodule)
-
-    result = trainer.predict(model,datamodule=datamodule)
-    submission = pd.read_csv("/opt/ml/input/data/sample_submission.csv")
-    submission['prediction'] = result[0]
-
-    now = datetime.now()
-    file_name = now.strftime('%m%d_%H%M%S_') + 'LightGCN' + "_submit.csv"
-
-    write_path = os.path.join('outputs/', file_name)
-    os.makedirs(name='outputs/', exist_ok=True)
-
-    submission.to_csv(write_path, index=False)
-    print(f"Successfully saved submission as {write_path}")
+    dm = GraphDataModule()
+    trainer = Trainer(dm)
+    trainer.train()
 
 if __name__ == "__main__":
     main()
