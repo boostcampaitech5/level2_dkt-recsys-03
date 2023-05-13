2b838eb39f19152cdb3079ee3efd96987be69c52
juhyein <hyeinju304@gmail.com>
2023-05-13 08:23:43 +0000

    chore: change directory files except for main

--- graph/graph/preprocess.py
+++ graph/graph/preprocess.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-from typing import Tuple
+from typing import Tuple, Dict
 import torch
 
 
@@ -10,7 +10,6 @@ def prepare_dataset( data_dir: str) -> Tuple[dict, dict, int]:
     id2index: dict = indexing_data(data=data)
     train_data_proc = process_data(data=train_data, id2index=id2index)
     test_data_proc = process_data(data=test_data, id2index=id2index)
-
     return train_data_proc, test_data_proc, len(id2index)
 
 def load_data(data_dir: str) -> pd.DataFrame: 
@@ -18,31 +17,29 @@ def load_data(data_dir: str) -> pd.DataFrame:
     path2 = os.path.join(data_dir, "test_data.csv")
     data1 = pd.read_csv(path1)
     data2 = pd.read_csv(path2)
-
     data = pd.concat([data1, data2])
     data.drop_duplicates(subset=["userID", "assessmentItemID"], keep="last", inplace=True)
     return data
 
-def indexing_data(data: pd.DataFrame) -> dict:
+def indexing_data(data: pd.DataFrame) -> Dict[str, int]:
     userid, itemid = (
         sorted(list(set(data.userID))),
         sorted(list(set(data.assessmentItemID))),
     )
-    n_user, n_item = len(userid), len(itemid)
-
+    n_user = len(userid)
     userid2index = {v: i for i, v in enumerate(userid)}
     itemid2index = {v: i + n_user for i, v in enumerate(itemid)}
     id2index = dict(userid2index, **itemid2index)
     return id2index
 
-
 def process_data(data: pd.DataFrame, id2index: dict) -> dict:
     edge, label = [], []
+
     for user, item, acode in zip(data.userID, data.assessmentItemID, data.answerCode):
         uid, iid = id2index[user], id2index[item]
         edge.append([uid, iid])
         label.append(acode)
-
+        
     edge = torch.LongTensor(edge).T
     label = torch.LongTensor(label)
     return dict(edge=edge,
@@ -52,4 +49,3 @@ def separate_data(data: pd.DataFrame) -> Tuple[pd.DataFrame]:
     train_data = data[data.answerCode >= 0]
     test_data = data[data.answerCode < 0]
     return train_data, test_data
-
