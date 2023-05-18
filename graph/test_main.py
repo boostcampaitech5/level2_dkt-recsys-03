b6ea3872efdeba71618b90b24406f45758f52ce6
2jun0 <soo28819@gmail.com>
2023-05-18 12:06:30 +0000

    fix: add wandb mocking in test graph

--- graph/test_main.py
+++ graph/test_main.py
@@ -2,12 +2,12 @@ from unittest.mock import patch, MagicMock
 from main import main
 
 
-# @patch('wandb.init')
-# @patch('wandb.login')
-# @patch('wandb.log')
-# @patch('wandb.finish')
-# @patch('wandb.save')
-# @patch("pytorch_lightning.loggers.wandb.Run", new=MagicMock)
-# @patch("pytorch_lightning.loggers.wandb.wandb")
+@patch('wandb.init')
+@patch('wandb.login')
+@patch('wandb.log')
+@patch('wandb.finish')
+@patch('wandb.save')
+@patch("pytorch_lightning.loggers.wandb.Run", new=MagicMock)
+@patch("pytorch_lightning.loggers.wandb.wandb")
 def test_main(*args, **kwargs):
     main()
