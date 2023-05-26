37da428c5fbf324d8d796be05b9bc379aa578fd6
2jun0 <soo28819@gmail.com>
2023-05-26 03:32:50 +0000

    refactor: use origin test_data csv

--- graph/graph/preprocess.py
+++ graph/graph/preprocess.py
@@ -8,9 +8,10 @@ from omegaconf import DictConfig
 
 def load_data(data_dir: str) -> pd.DataFrame:
     path1 = os.path.join(data_dir, "train_data.csv")
-    path2 = os.path.join(data_dir, "test_valid.csv")
+    path2 = os.path.join(data_dir, "test_data.csv")
     train = pd.read_csv(path1)
     test = pd.read_csv(path2)
+    test = get_valid(test)
 
     data = pd.concat([train, test])
     data.drop_duplicates(subset=["userID", "assessmentItemID"], keep="last", inplace=True)
@@ -49,7 +50,7 @@ def separate_data(data: pd.DataFrame) -> Tuple[pd.DataFrame]:
 
 def get_valid(data: pd.DataFrame) -> pd.DataFrame:
     data["idx"] = np.arange(0, data.shape[0])
-    data = data[data.answerCode >= 0]
-    id_list = data.groupby("userID").last().idx
-    data.loc[id_list, "answerCode"] = -1
+
+    id_list = data.groupby("userID").nth(-2).idx
+    data.loc[id_list, "answerCode"] = -2
     return data
